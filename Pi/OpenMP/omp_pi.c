f2d9fa34bfa1e1c7f7adc284c164d046c3aa9cc1
Mrigank Doshy <mrigankdoshy@gmail.com>
2019-07-01 21:30:06 -0400

    Update omp_pi.c

--- Pi/OpenMP/omp_pi.c
+++ Pi/OpenMP/omp_pi.c
@@ -1,35 +1,34 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include<time.h>
 
-int main (int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  int i, count, samples, nthreads, seed;
-  struct drand48_data drand_buf;
-  double x, y;
-  double t0, t1;
+  int iterations = 100000;         //number of iterations per FOR loop
+  double x, y, z;                  //x,y value for the random coordinate. z is used to check if x^2+y^2<=1            
+  int count=0;                     //count holds all the number of how many good coordinates
+  double pi;                       //holds approx value of pi
+  int numthreads = 16;
+  int i;
 
-  samples  = atoi(argv[1]);
-  nthreads = atoi(argv[2]);
-  omp_set_num_threads (nthreads);
+  #pragma omp parallel firstprivate(x, y, z, i) reduction(+:count) num_threads(numthreads)
+  {
+    srand48((int)time(NULL) ^ omp_get_thread_num());  //Give random() a seed value
+    for (i = 0; i<iterations; ++i)         
+    {
+      x = (double)drand48();
+      y = (double)drand48();
+      z = ((x * x) + (y * y));
+      
+      if (z <= 1)
+        ++count;
+    }
+  } 
 
-  t0 = omp_get_wtime();
-  count = 0;
-
-#pragma omp parallel private(i, x, y, seed, drand_buf) shared(samples)
-{
-  seed = 1202107158 + omp_get_thread_num() * 1999;
-  srand48_r (seed, &drand_buf);
-
-#pragma omp for reduction(+:count)
-  for (i=0; i<samples; i++) {
-    drand48_r (&drand_buf, &x);
-    drand48_r (&drand_buf, &y);
-    if (x*x + y*y <= 1.0) count++;
-  }
-}
-
-  t1 = omp_get_wtime();
-  printf("Estimate of pi: %7.5f\n", 4.0*count/samples);
-  printf("Time: %7.2f\n", t1-t0);
+  pi = ((double)count / (double)(iterations * numthreads)) * 4.0;
+  printf("Estimated Pi: %f\n", pi);
+  
+  return 0;
 }
