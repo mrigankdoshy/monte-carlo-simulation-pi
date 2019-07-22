ac3805cd3bed0a9d2182ce462faaf25ab9d38f67
Mrigank Doshy <mrigankdoshy@gmail.com>
2019-07-22 13:05:00 -0400

    Update mpi_reduction.c

--- Pi/MPI/mpi_reduction.c
+++ Pi/MPI/mpi_reduction.c
@@ -4,47 +4,47 @@
 #include <time.h>
 #include <mpi.h>
 
-int main(int argc, char* argv[])
-{
-	int iterations = 100000;
-	int myid;									//hold's process's rank id
-	double x,y;									//x,y value for the random coordinate
-	int i;
-    int count=0;								//Count holds all the number of how many good coordinates
-	double z;									//Used to check if x^2+y^2<=1
-	double pi;									//holds approx value of pi
-	int numnodes;
-	int reducedcount;							//total number of "good" points from all nodes
+int main(int argc, char* argv[]) {
+	int pid, numberOfNodes, i, sum, error, count = 0, iterations = 100000000;								
+	double x, y, z, pi, begin, end;																
 
-	MPI_Init(&argc, &argv);						//Start MPI
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid);		//get rank of node's process
-	MPI_Comm_size(MPI_COMM_WORLD, &numnodes);
+	/* 
+    	1. Get process ID
+    	2. Get processes number
+    	3. Synchonize all processes
+    	4. Get begin time
+    */
+	error = MPI_Init(&argc, &argv);						
+	MPI_Comm_rank(MPI_COMM_WORLD, &pid);		
+	MPI_Comm_size(MPI_COMM_WORLD, &numberOfNodes);
+	begin = MPI_Wtime();
 
-	srand48(time(NULL)+myid);
+	srand48(time(NULL) + pid);
 
-	for(i = 0; i < iterations; i++)
-	{
-		x = (double)drand48();					//gets a random x coordinate
-		y = (double)drand48();					//gets a random y coordinate
-		z = ((x*x)+(y*y));						//Checks to see if number in inside unit circle
-		if (z<=1)
+	for(i = 0; i < iterations; i++) {
+		x = (double)drand48();					
+		y = (double)drand48();					
+		z = ((x*x)+(y*y));						
+		
+		if (z <= 1)
 		{
-			count++;							//if it is, consider it a valid random point	
+			count++;							
 		}	
 	}
 
 	/* Now we can reduce the values to master */
 
-	MPI_Reduce(&count, &reducedcount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-		
-	int total_iter = iterations * numnodes;
+	MPI_Reduce(&count, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	end = MPI_Wtime();
 
-	if (myid == 0)								//if root process
+	int total_iterations = iterations * numberOfNodes;
+
+	if (pid == 0)								//if root process
 	{      
-		//p = 4(m/n)
-		pi = ((double)reducedcount/(double)total_iter)*4.0;	
-		printf("Pi: %f\n", pi);			
+		pi = ((double)sum / (double)total_iterations) * 4.0;	
+		printf("Processors = %2d;    Time = %f s;    PI = %0.10f\n", numberOfNodes, end-begin, pi);			
 	}
-	MPI_Finalize();								//Close the MPI instance
+	
+	error = MPI_Finalize();								//Close the MPI instance
 	return 0;
 }
